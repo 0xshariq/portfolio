5386b139b97101b8159ef89abd1ce8ecfef979c0
Shariq Choudhary <khanshariq92213@gmail.com>
2024-12-18 21:48:34 +0530

    Minor Change In Contact Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,10 +144,10 @@ export default function Page() {
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Want to chat? Just shoot me a dm{" "}
                 <Link
-                  href={DATA.contact.social.X.url}
+                  href="https://www.instagram.com/sharique1303"
                   className="text-blue-500 hover:underline"
                 >
-                  with a direct question on twitter
+                  with a direct question on instagram
                 </Link>{" "}
                 and I&apos;ll respond whenever I can. I will ignore all
                 soliciting.
